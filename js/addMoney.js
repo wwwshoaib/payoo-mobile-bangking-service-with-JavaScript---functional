d723c9b222e58c3163297b2ea36d94e3c686bdac
Shoaib <shoaibchemistrysust@gmail.com>
2024-12-05 00:49:40 +0600

    validation added

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,23 +1,44 @@
 // add money button
 
 document.getElementById('btn-add-money')
-.addEventListener('click', function(event) {
-    event.preventDefault();
-    const addMoneyNumber = getInputFieldValueById('input-add-money');
-    const pinNumber = getInputFieldValueById('input-pin-number');
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const addMoneyNumber = getInputFieldValueById('input-add-money');
+        const pinNumber = getInputFieldValueById('input-pin-number');
+        if(isNaN(addMoneyNumber)) {
+            alert('Failed to cash out');
+            return;
+        }
     
-    // validation pin number (wrong way)
 
-    if(pinNumber === 1234) {
-        const balance = getTextFieldValueById('account-balance');
-        const balanceNew = balance + addMoneyNumber;
-        document.getElementById('account-balance').innerText = balanceNew;
+        // validation pin number (wrong way)
 
-    } 
+        if (pinNumber === 1234) {
+            const balance = getTextFieldValueById('account-balance');
+            const balanceNew = balance + addMoneyNumber;
+            document.getElementById('account-balance').innerText = balanceNew;
 
-    else {
-        alert('Failed adding money !');
-    }
-    
-    
-})
+            // add transaction history (without function)
+            const div = document.createElement('div');
+            p.innerHTML = `Credited ${addMoneyNumber} taka. C/B: ${balanceNew} taka`;
+            document.getElementById('transaction-id').appendChild(div);
+
+            // toast
+
+            <div class="toast toast-top toast-start">
+                <div class="alert alert-info">
+                    <span>New mail arrived.</span>
+                </div>
+                <div class="alert alert-success">
+                    <span>Message sent successfully.</span>
+                </div>
+            </div>
+
+        }
+
+        else {
+            alert('Failed adding money !');
+        }
+
+
+    })
--- js/cash-out.js
+++ js/cash-out.js
@@ -3,13 +3,33 @@ document.getElementById('btn-cash-out')
     event.preventDefault();
     const cashOut = getInputFieldValueById('input-cash-out');
     const pinNumber = getInputFieldValueById('input-cash-out-pin-number');
+
+    if(isNaN(cashOut)) {
+        alert('Failed to cash out');
+        return;
+    }
+
     
     // validation pin number (wrong way)
 
     if(pinNumber === 1234) {
         const balance = getTextFieldValueById('account-balance');
+
+        if(cashOut > balance) {
+            alert('Not enough money');
+            return;
+        }
         const balanceNew = balance - cashOut;
         document.getElementById('account-balance').innerText = balanceNew;
+
+        const div = document.createElement('div');
+        div.classList.add('bg-yellow-300');
+        div.innerHTML = `
+        <h4 class = "text-xl text-semibold">Cash Out: </h4>
+        <p> Debited ${cashOut} taka. C/B: ${balanceNew} taka
+
+        `
+        document.getElementById('transaction-id').appendChild(div);
 
     } 
 
