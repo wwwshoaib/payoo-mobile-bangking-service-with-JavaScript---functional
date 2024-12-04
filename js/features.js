c75674a8810b18fdadfa6c6829328998c2df5a03
Shoaib <shoaibchemistrysust@gmail.com>
2024-12-04 23:33:37 +0600

    toggles added

---
+++ js/features.js
@@ -0,0 +1,31 @@
+
+//show add money form
+
+document.getElementById('btn-show-add-money-form')
+.addEventListener('click', function(){
+
+showSectionById('show-form-add-money');
+
+
+}) 
+
+//show cash out form
+
+document.getElementById('btn-show-cash-form')
+.addEventListener('click', function(){
+
+showSectionById('show-form-cash-out');
+
+
+}) 
+
+
+// show transaction section
+
+document.getElementById('btn-show-transaction')
+.addEventListener('click', function(){
+
+showSectionById('show-transaction-section');
+
+
+})
--- js/utlities.js
+++ js/utlities.js
@@ -12,4 +12,15 @@ function getTextFieldValueById(id) {
     const textValue = document.getElementById(id).innerText;
     const textNumber = parseFloat(textValue);
     return textNumber;
+} 
+
+// function to show a section by hidding other sections
+
+function showSectionById(id) {
+    document.getElementById('show-form-add-money').classList.add('hidden');
+    document.getElementById('show-form-cash-out').classList.add('hidden');
+    document.getElementById('show-transaction-section').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden');
+
 }
